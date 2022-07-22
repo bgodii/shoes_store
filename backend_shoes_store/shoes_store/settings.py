1a93521f0b7aab5092a5dce2a8f3c7995b1a4dec
renan dantas <reehdantascarvalho@gmail.com>
2022-07-21 23:16:37 -0300

    feat: setting up token retrieve

--- backend_shoes_store/shoes_store/settings.py
+++ backend_shoes_store/shoes_store/settings.py
@@ -144,6 +144,7 @@ STATIC_ROOT = os.path.join(ROOT_DIR, "static")
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ),

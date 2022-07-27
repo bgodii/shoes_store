87df12ffc9cf965534c93938611e078cef418ea8
renan dantas <reehdantascarvalho@gmail.com>
2022-07-27 01:42:56 -0300

    feat: improve form

--- frontend_shoes_store/src/App.js
+++ frontend_shoes_store/src/App.js
@@ -1,12 +1,13 @@
 import 'bulma/css/bulma.min.css';
 import InputData from './components/inputData';
-
+import Table from './components/table'
 
 function App() {
   return (
     <div style={{ marginTop: 10, backgroundColor: "#f2f2f2" }}>
       <div className="container">
         <InputData />
+        <Table style={{ paddingTop: 12 }} />
       </div>
     </div>
   );
--- frontend_shoes_store/src/components/inputData/index.js
+++ frontend_shoes_store/src/components/inputData/index.js
@@ -3,61 +3,116 @@ import API from "../../infra/axios"
 
 const InputData = () => {
     const [values, setValues] = useState({})
+    const [isUpload, setUpload] = useState(false)
+    const [file, setFile] = useState()
 
     const handlechange = (event) => {
         setValues({ ...values, [event.target.name]: event.target.value.toLowerCase() })
     }
 
     const sendData = async () => {
-        const response = await API.postData(values, '/api/shoes/')
+        if (!isUpload) {
+            response = await API.postData(values, '/api/shoes/')
+        } else {
+            response = await API.uploadFile(file["file"], '/api/upload/')
+        }
+
+        window.location.reload(true)
+    }
+
+    const changeHeader = (event) => {
+        if (event.target.name == "form") {
+            setUpload(false)
+        }
+        else {
+            setUpload(true)
+        }
+    }
+
+    const onFileChange = (event) => {
+        let formData = new FormData()
+        formData.append("file", event.target.files[0])
+        const fileName = event.target.files[0].name
+        setFile({ "file": formData, "name": fileName })
     }
 
     return (
-        <div className="columns">
-            <div className="column is-narrow">
-                <input className="input is-small" type="text" placeholder="modelo" name="model" onChange={e => handlechange(e)} />
+        <>
+            <div className="tabs">
+                <ul>
+                    <li className={!isUpload ? "is-active" : ""}><a onClick={changeHeader} name="form">Fill Form</a></li>
+                    <li className={isUpload ? "is-active" : ""}><a onClick={changeHeader} name="upload">Upload File</a></li>
+                </ul>
             </div>
-            <div className="column is-narrow">
-                <input className="input is-small" type="text" placeholder="tamanho" name="size" onChange={e => handlechange(e)} />
-            </div>
-            <div className="column is-narrow">
-                <div className="select is-small">
-                    <select name="gender" onChange={e => handlechange(e)}>
-                        <option disabled selected hidden>Genero</option>
-                        <option>Male</option>
-                        <option>Female</option>
-                        <option>Unissex</option>
-                    </select>
+            {isUpload ?
+                <div className="file has-name is-small is-fullwidth is-link" style={{ paddingBottom: 12 }}>
+                    <label className="file-label">
+                        <input className="file-input" type="file" name="resume" onChange={onFileChange} />
+                        <span className="file-cta">
+                            <span className="file-icon">
+                                <i className="fas fa-upload"></i>
+                            </span>
+                            <span className="file-label">
+                                Choose a file
+                            </span>
+                        </span>
+                        <span className="file-name">
+                            {!file ? "file name" : file.name}
+                        </span>
+
+                    </label>
                 </div>
+                : (
+
+                    <div className="columns" style={{ marginBottom: 0 }}>
+                        <div className="column is-narrow">
+                            <input className="input is-small" type="text" placeholder="modelo" name="model" onChange={e => handlechange(e)} />
+                        </div>
+                        <div className="column is-narrow">
+                            <input className="input is-small" type="text" placeholder="tamanho" name="size" onChange={e => handlechange(e)} />
+                        </div>
+                        <div className="column is-narrow">
+                            <div className="select is-small">
+                                <select name="gender" onChange={e => handlechange(e)}>
+                                    <option disabled selected hidden>Genero</option>
+                                    <option>Male</option>
+                                    <option>Female</option>
+                                    <option>Unissex</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="column is-narrow">
+                            <input className="input is-small" type="text" placeholder="cor" name="color" onChange={e => handlechange(e)} />
+                        </div>
+                        <div className="column is-narrow">
+                            <input className="input is-small" type="text" placeholder="marca" name="brand" onChange={e => handlechange(e)} />
+                        </div>
+                        <div className="column is-narrow">
+                            <div className="select is-small">
+                                <select name="type" onChange={e => handlechange(e)}>
+                                    <option disabled selected hidden>Estilo</option>
+                                    <option>Botas</option>
+                                    <option>Casual</option>
+                                    <option>Sneaker</option>
+                                    <option>Formal</option>
+                                    <option>Sandalias</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="column is-narrow">
+                            <input className="input is-small" type="text" placeholder="preço" name="price" onChange={e => handlechange(e)} />
+                        </div>
+                        <div className="column is-narrow">
+                            <input className="input is-small" type="text" placeholder="quantidade" name="quantity" onChange={e => handlechange(e)} />
+                        </div>
+                    </div>
+                )}
+            <div className="column is-narrow" style={{ margin: 0, padding: 0 }}>
+                <button className="button is-link is-responsive is-fullwidth" onClick={sendData} style={{ marginBottom: 12, }}>Enviar</button>
             </div>
-            <div className="column is-narrow">
-                <input className="input is-small" type="text" placeholder="cor" name="color" onChange={e => handlechange(e)} />
-            </div>
-            <div className="column is-narrow">
-                <input className="input is-small" type="text" placeholder="marca" name="brand" onChange={e => handlechange(e)} />
-            </div>
-            <div className="column is-narrow">
-                <div className="select is-small">
-                    <select name="type" onChange={e => handlechange(e)}>
-                        <option disabled selected hidden>Estilo</option>
-                        <option>Botas</option>
-                        <option>Casual</option>
-                        <option>Sneaker</option>
-                        <option>Formal</option>
-                        <option>Sandalias</option>
-                    </select>
-                </div>
-            </div>
-            <div className="column is-narrow">
-                <input className="input is-small" type="text" placeholder="preço" name="price" onChange={e => handlechange(e)} />
-            </div>
-            <div className="column is-narrow">
-                <input className="input is-small" type="text" placeholder="quantidade" name="quantity" onChange={e => handlechange(e)} />
-            </div>
-            <div className="column is-narrow">
-                <button className="button is-link is-small" onClick={sendData}>Enviar</button>
-            </div>
-        </div>
+
+        </>
+
     )
 }
 
